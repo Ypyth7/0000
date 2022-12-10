b6d3fbc38597f80197251670962df23bcf08dd49
Ypyth7 <120253016+Ypyth7@users.noreply.github.com>
2022-12-10 21:25:00 +0700

    Add files via upload

---
+++ Application1.py
@@ -0,0 +1,7 @@
+Y=0
+import time
+time.sleep(3)
+while True:
+    time.sleep(1)
+    Y=Y+1
+    print(Y)
---
+++ программа записи.py
@@ -0,0 +1,10 @@
+print("                                                                           программа записи")
+name = input("имя?: ")
+fam = input("фамилия?: ")
+it = input ("очество-имя отца: ")
+print(name)
+print(fam)
+print(it)
+for x in range(0,5):
+    print("запись...")
+
